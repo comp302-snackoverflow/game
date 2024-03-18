aa6f756d206eccc33d9c88d641a9f686d6ea0367
Damla <damdam@Damdam-MacBook-Air.local>
2024-03-18 12:11:34 +0300

    -Add the new Java Project

---
+++ game_artifact/src/main/java/tr/edu/ku/comp302/App.java
@@ -0,0 +1,13 @@
+package tr.edu.ku.comp302;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
