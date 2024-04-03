2162fa758b964030e0ad99c5379f6ab18c1d4660
ayazici21 <100401653+ayazici21@users.noreply.github.com>
2024-04-03 12:19:18 +0300

    Test

--- game_artifact/src/main/java/tr/edu/ku/comp302/App.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/App.java
@@ -6,8 +6,8 @@ package tr.edu.ku.comp302;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
