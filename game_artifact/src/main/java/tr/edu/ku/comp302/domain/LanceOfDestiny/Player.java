f046c5adc2a21366a832a1f3dd4ac3ad6f8323c1
ayazici21 <ayazici21@ku.edu.tr>
2024-04-04 15:32:54 +0300

    Revert "Implement Player.java"
    This reverts commit 17f1a37bc4dc4f0071f9aa20a4147336f96faf2a.

--- game_artifact/src/main/java/tr/edu/ku/comp302/domain/LanceOfDestiny/Player.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/domain/LanceOfDestiny/Player.java
@@ -1,27 +1,5 @@
 package tr.edu.ku.comp302.domain.LanceOfDestiny;
 
 public class Player {
-    private final int ID;
-    private final String name;
-
-    public Player(int ID, String name) {
-        this.name = name;
-        this.ID = ID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "ID=" + ID +
-                ", name='" + name + '\'' +
-                '}';
-    }
+    
 }
