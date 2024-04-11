e83b829b462a4efa33bdda97bd81fa59b47882c9
ayazici21 <ayazici21@ku.edu.tr>
2024-04-11 17:30:55 +0300

    Set up application entry point.

--- game_artifact/src/main/java/tr/edu/ku/comp302/App.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/App.java
@@ -1,24 +1,13 @@
 package tr.edu.ku.comp302;
 
+import tr.edu.ku.comp302.ui.frame.MainFrame;
 
-import tr.edu.ku.comp302.domain.entity.Lance;
-import tr.edu.ku.comp302.domain.lanceofdestiny.LanceOfDestiny;
-import tr.edu.ku.comp302.domain.lanceofdestiny.Level;
-import tr.edu.ku.comp302.ui.panel.LevelPanel;
-import tr.edu.ku.comp302.ui.view.LanceView;
-
-/**
- * Hello world!
- */
 public class App 
 {
     public static void main(String[] args)
     {
-        Lance lance = new Lance(576, 600);
-        LanceView lanceView = new LanceView(lance);
-        Level level = new Level();
-        LevelPanel levelPanel = new LevelPanel(level, lanceView);
-        new LanceOfDestiny(levelPanel);
-
+        MainFrame frame = MainFrame.createMainFrame();
+        frame.showLoginPanel();
+        frame.setVisible(true);
     }
 }
