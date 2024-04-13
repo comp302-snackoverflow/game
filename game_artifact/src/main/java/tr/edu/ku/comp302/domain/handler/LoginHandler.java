922d00c23caa4d92f3e24bfbda3eec15340dc3e2
Mustafa-Meric-Kasap <mkasap20@ku.edu.tr>
2024-04-13 19:39:43 +0300

    Logger object added to RegisterHandler.java and LoginHandler.java

--- game_artifact/src/main/java/tr/edu/ku/comp302/domain/handler/LoginHandler.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/domain/handler/LoginHandler.java
@@ -1,5 +1,7 @@
 package tr.edu.ku.comp302.domain.handler;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import tr.edu.ku.comp302.domain.services.Hash;
 
 public class LoginHandler {
@@ -10,6 +12,7 @@ public class LoginHandler {
     public static final int PASSWORD_EMPTY = 2;
     public static final int USER_NOT_FOUND = 3;
 
+    private static final Logger logger = LogManager.getLogger();
     private LoginHandler() {
         dbHandler = DatabaseHandler.getInstance();
     }
@@ -22,13 +25,15 @@ public class LoginHandler {
         String salt = dbHandler.getSaltByUsername(username);
 
         if (salt == null) {
+            logger.debug("User not found");
             return USER_NOT_FOUND;
         }
         String hash = Hash.hash(password, salt);
         if (dbHandler.validateLogin(username, hash)) {
+            logger.info(String.format("Player %s successfully logged in", username));
             return SUCCESS;
         }
-
+        logger.debug(String.format("User with username %s not found", username));
         return USER_NOT_FOUND;
     }
 
@@ -41,12 +46,13 @@ public class LoginHandler {
 
     private int validateInput(String uname, String password) {
         if (uname.isEmpty()) {
+            logger.debug("Username is empty");
             return USERNAME_EMPTY;
         }
         if (password.isEmpty()) {
+            logger.debug("Password is empty");
             return PASSWORD_EMPTY;
         }
-
         return SUCCESS;
     }
 }
