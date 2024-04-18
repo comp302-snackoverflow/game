67ff6ff7f34e69581de1473a34f40a7b97f7a4ce
Mustafa-Meric-Kasap <mkasap20@ku.edu.tr>
2024-04-18 16:53:18 +0300

    UPS dependency added to the rotation logic

--- game_artifact/src/main/java/tr/edu/ku/comp302/domain/lanceofdestiny/LanceOfDestiny.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/domain/lanceofdestiny/LanceOfDestiny.java
@@ -1,8 +1,11 @@
 package tr.edu.ku.comp302.domain.lanceofdestiny;
 
+
+import tr.edu.ku.comp302.domain.entity.Lance;
 import tr.edu.ku.comp302.domain.handler.KeyboardHandler;
 import tr.edu.ku.comp302.ui.frame.MainFrame;
 import tr.edu.ku.comp302.ui.panel.LevelPanel;
+
 
 public class LanceOfDestiny implements Runnable{
 
@@ -23,12 +26,7 @@ public class LanceOfDestiny implements Runnable{
     public void run() {
         double timePerFrame = 1_000_000_000.0 / FPS_SET;
         double timePerUpdate = 1_000_000_000.0 / UPS_SET;
-
         long previousTime = System.nanoTime();
-        long[] keyPressTimes = new long[2]; // [keyPressTimeA, keyPressTimeD]
-        long releaseTime = 0;
-        double[] remainderHolder = new double[2];
-        double steadyStateTime = 0.0;
         int frames = 0;
         int updates = 0;
         long[] arrowKeyPressTimes = new long[2];    // [rightArrowKeyPressTime, leftArrowKeyPressTime]
@@ -50,32 +48,16 @@ public class LanceOfDestiny implements Runnable{
                         arrowKeyPressTimes, 1, currentTime, speed, totalArrowKeyPressTimes);
 
                 // Handle rotation logic for 'A' key
-                handleRotationLogic(KeyboardHandler.buttonAPressed, keyPressTimes, currentTime, 0, -0.4, remainderHolder);
+                handleRotationLogic(KeyboardHandler.buttonAPressed && !KeyboardHandler.buttonDPressed, -20.0);
 
                 // Handle rotation logic for 'D' key
-                handleRotationLogic(KeyboardHandler.buttonDPressed, keyPressTimes, currentTime, 1, 0.4, remainderHolder);
+                handleRotationLogic(KeyboardHandler.buttonDPressed && !KeyboardHandler.buttonAPressed, 20.0);
 
-
-                if (KeyboardHandler.buttonAPressed == KeyboardHandler.buttonDPressed) {
-                        if (releaseTime == 0){
-                            releaseTime = System.nanoTime();
-                        }else{
-                            long elapsedTime = currentTime - releaseTime;
-                            double elapsedReleaseMs = (double) elapsedTime / 1_000_000.0;
-                            steadyStateTime += elapsedReleaseMs;
-                            if (steadyStateTime >= 10){
-                                levelPanel.getLanceView().getLance().stayInSteadyState(0.45);
-                                steadyStateTime -= 10;
-                                releaseTime = 0;
-                            }
-                        }
-                }else{
-                    releaseTime = 0;
-                }
+                boolean steadyStateBoolean = KeyboardHandler.buttonAReleased && KeyboardHandler.buttonDReleased ||
+                                                (KeyboardHandler.buttonAPressed && KeyboardHandler.buttonDPressed);
+                handleSteadyStateLogic(steadyStateBoolean, 45);
                 updates++;
                 deltaUpdate--;
-
-
             }
             if (deltaFrame >= 1){
                 levelPanel.repaint();
@@ -90,15 +72,15 @@ public class LanceOfDestiny implements Runnable{
         if (arrowKeyPressed) {
             if (keyPressTimes[index] == 0) {
                 keyPressTimes[index] = currentTime;
-            } else {
-                long elapsedTime = currentTime - keyPressTimes[index];
-                double elapsedMs = (double) elapsedTime / 1_000_000.0 + remainderHolder[index];
-                int minIntPxMovement = calculateMinIntegerPxMovement();
-                if (elapsedMs >= minIntPxMovement / speed) {
-                    levelPanel.getLanceView().moveLance(minIntPxMovement);
-                    keyPressTimes[index] = 0;
-                    remainderHolder[index] -= minIntPxMovement / speed;
-                }
+            }
+            long elapsedTime = currentTime - keyPressTimes[index];
+            double elapsedMs = ((double) elapsedTime / 1_000_000.0) + remainderHolder[index];
+            int minPx = calculateMinIntegerPxMovement();
+            double minMsToMove =  minPx * 1000.0 / speed;
+            if (elapsedMs >= minMsToMove){
+                remainderHolder[index] = elapsedMs - minMsToMove;
+                keyPressTimes[index] = System.nanoTime();
+                levelPanel.getLanceView().moveLance(minPx);
             }
         } else {
             keyPressTimes[index] = 0;
@@ -106,23 +88,22 @@ public class LanceOfDestiny implements Runnable{
         }
     }
 
-    private void handleRotationLogic(boolean keyPressed, long[] keyPressTimes, long currentTime, int index, double degrees, double[] remainderHolder) {
+    private void handleRotationLogic(boolean keyPressed, double angularSpeed) {
         if (keyPressed) {
-            if (keyPressTimes[index] == 0) {
-                keyPressTimes[index] = currentTime;
-            } else {
-                long elapsedTime = currentTime - keyPressTimes[index];
-                double elapsedMs = (double) elapsedTime / 1_000_000.0 + remainderHolder[index];
-                if (elapsedMs >= 20) {
-                    levelPanel.getLanceView().rotateLance(degrees);
-                    keyPressTimes[index] = 0;
-                    remainderHolder[index] = elapsedMs - 20;
-                }
-            }
-        } else {
-            keyPressTimes[index] = 0;
-            remainderHolder[index] = 0;
+            double angularChange = calculateAngularChangePerUpdate(angularSpeed);
+            levelPanel.getLanceView().rotateLance(angularChange);
         }
+    }
+
+    private void handleSteadyStateLogic(boolean keyPressed, double angularSpeed){
+        if (keyPressed){
+            double angularChange = calculateAngularChangePerUpdate(angularSpeed);
+            levelPanel.getLanceView().getLance().stayInSteadyState(angularChange);
+        }
+    }
+
+    private double calculateAngularChangePerUpdate(double angularSpeed){
+        return angularSpeed * getMsPerUpdate() / 1000.0;
     }
 
     private int calculateMinIntegerPxMovement(){
@@ -135,6 +116,7 @@ public class LanceOfDestiny implements Runnable{
             return ((int) pxPerUpdate) + 1;
         }
     }
+
     private double getMsPerUpdate(){
         return 1000.0 / UPS_SET;
     }
--- game_artifact/src/main/java/tr/edu/ku/comp302/ui/view/LanceView.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/ui/view/LanceView.java
@@ -36,8 +36,6 @@ public class LanceView {
         lance.incrementRotationAngle(degrees);
     }
 
-
-
     public Lance getLance() {
         return lance;
     }
