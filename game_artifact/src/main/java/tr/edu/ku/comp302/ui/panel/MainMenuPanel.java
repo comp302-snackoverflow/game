54dd02a95e043cdeb376835aeb96840015d85535
Ömer Burak Duran <omerburakduran@gmail.com>
2024-04-25 16:48:16 +0300

    Implemented the MainMenuPanel and PauseMenuPanel classes in ui/panel package.

---
+++ game_artifact/src/main/java/tr/edu/ku/comp302/ui/panel/MainMenuPanel.java
@@ -0,0 +1,81 @@
+package tr.edu.ku.comp302.ui.panel;
+
+import javax.swing.*;
+import java.awt.*;
+import tr.edu.ku.comp302.domain.lanceofdestiny.LanceOfDestiny;
+import tr.edu.ku.comp302.domain.lanceofdestiny.GameState;
+
+public class MainMenuPanel extends JPanel {
+    protected JButton newGameButton;
+    protected JButton loadGameButton;
+    protected JButton createCustomMapButton;
+    protected JButton helpButton;
+    protected JButton optionsButton;
+    protected JButton quitButton;
+    public MainMenuPanel() {
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        setLayout(gridBagLayout);
+
+        newGameButton = new JButton("New Game");
+        loadGameButton = new JButton("Load Game");
+        createCustomMapButton = new JButton("Create Custom Map");
+        helpButton = new JButton("Help");
+        optionsButton = new JButton("Options");
+        quitButton = new JButton("Quit");
+
+        newGameButton.addActionListener(e -> {
+            LanceOfDestiny.setCurrentGameState(GameState.NEW_GAME);
+        });
+        loadGameButton.addActionListener(e -> {
+            LanceOfDestiny.setCurrentGameState(GameState.LOAD_GAME);
+        });
+        createCustomMapButton.addActionListener(e -> {
+            LanceOfDestiny.setCurrentGameState(GameState.CREATE_CUSTOM_MAP);
+        });
+        helpButton.addActionListener(e -> {
+            LanceOfDestiny.setCurrentGameState(GameState.HELP);
+
+        });
+        optionsButton.addActionListener(e -> {
+            LanceOfDestiny.setCurrentGameState(GameState.OPTIONS);
+            System.out.println("Options");
+        });
+        quitButton.addActionListener(e -> {
+            System.exit(0);
+            // TODO: Change this later.
+        });
+
+        Dimension buttonSize = new Dimension(150, 30);
+        newGameButton.setPreferredSize(buttonSize);
+        loadGameButton.setPreferredSize(buttonSize);
+        createCustomMapButton.setPreferredSize(buttonSize);
+        helpButton.setPreferredSize(buttonSize);
+        optionsButton.setPreferredSize(buttonSize);
+        quitButton.setPreferredSize(buttonSize);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        this.add(newGameButton, gbc);
+
+        gbc.gridy = 1;
+        this.add(loadGameButton, gbc);
+
+        gbc.gridy = 2;
+        this.add(createCustomMapButton, gbc);
+
+        gbc.gridy = 3;
+        this.add(helpButton, gbc);
+
+        gbc.gridy = 4;
+        this.add(optionsButton, gbc);
+
+        gbc.gridy = 5;
+        this.add(quitButton, gbc);
+    }
+}
