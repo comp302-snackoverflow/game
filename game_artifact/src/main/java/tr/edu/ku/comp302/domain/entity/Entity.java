c850077a4e7aac2c8eddc582e0c56fbc2016acb3
yomur21 <yomur21@ku.edu.tr>
2024-05-18 17:24:09 +0300

    Wrote the overview, abstraction function, rep invariant, and the repOk function for the Entity class.

--- game_artifact/src/main/java/tr/edu/ku/comp302/domain/entity/Entity.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/domain/entity/Entity.java
@@ -3,6 +3,24 @@ package tr.edu.ku.comp302.domain.entity;
 import java.awt.geom.Rectangle2D;
 
 public abstract class Entity {
+    // OVERVIEW: This class represents a general game entity, which has a position, and a bounding box
+    // to check for collisions. It provides some setter and getter methods, and also provides a method
+    // to update an entity's position as the user resizes the screen.
+
+    // ABSTRACTION FUNCTION:
+    // A typical entity is going to be in the form of (x, y) + boundingBox, where x is the x coordinate
+    // of the entity, and y is the y coordinate of the entity. Its abstraction function can be described as:
+    // AF(c) = (c.xPosition, c.yPosition) & c.boundingBox.
+
+    // REP INVARIANT:
+    // The representation invariant of an Entity class should be as follows:
+    // c.xPosition >= 0 and c.yPosition >= 0
+    // c.boundingBox should not be null.
+    // c.boundingBox.getX() = c.xPosition
+    // c.boundingBox.getY() = c.yPosition
+    // In brief, the x and y coordinates cannot be negative, the bounding box should not be null,
+    // and if the position of the Entity is updated, the bounding box must be updated accordingly at all times.
+
     protected double xPosition;
     protected double yPosition;
     protected Rectangle2D boundingBox;
@@ -37,6 +55,16 @@ public abstract class Entity {
 
     public Rectangle2D getBoundingBox() {
         return boundingBox;
+    }
+
+    public boolean repOk() {
+        // EFFECTS: Returns true if the representation invariant of the Entity class holds true,
+        // otherwise returns false.
+        if (this.xPosition < 0 || this.yPosition < 0) return false;
+        if (this.boundingBox == null) return false;
+        if (this.boundingBox.getX() != this.xPosition || this.boundingBox.getY() != this.yPosition) return false;
+
+        return true;
     }
 
 }
