68c9b613c0415bc1eef548d3a820ce219205ed4e
ayazici21 <ayazici21@ku.edu.tr>
2024-06-01 15:41:56 +0300

    Why does this happen?!

--- game_artifact/src/main/java/tr/edu/ku/comp302/App.java
+++ game_artifact/src/main/java/tr/edu/ku/comp302/App.java
@@ -1,10 +1,7 @@
 package tr.edu.ku.comp302;
 
 import tr.edu.ku.comp302.domain.handler.DatabaseHandler;
-import tr.edu.ku.comp302.domain.handler.LevelHandler;
-import tr.edu.ku.comp302.domain.lanceofdestiny.LanceOfDestiny;
 import tr.edu.ku.comp302.ui.frame.MainFrame;
-import tr.edu.ku.comp302.ui.panel.LevelPanel;
 
 public class App {
     public static void main(String[] args) {
